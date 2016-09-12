2b0a5c3c71032e5d6ac050d991ac12c3a0160183
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-12 16:40:21 -0400

    adds categories and announcements

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'tags/:tag', to: 'posts#index', as: :tag
+  get 'posts/:tag', to: 'posts#index', as: :tag
   resources :posts
 
   root :to => 'posts#main'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#  do
+#   collection do
+#     get :announcements
+#     get :events
+#     get :news
+#     get :talents
+#   end
+# end
