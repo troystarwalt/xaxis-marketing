6f1da159ff386eed9663d5f9105814b99267dd98
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-22 10:54:04 -0400

    updates migration file

--- db/migrate/20160906193035_rename_update_table.rb
+++ db/migrate/20160906193035_rename_update_table.rb
@@ -1,5 +1,5 @@
-# class RenameUpdateTable < ActiveRecord::Migration[5.0]
+class RenameUpdateTable < ActiveRecord::Migration[5.0]
 #   def change
 #     rename_table :updates, :posts
 #   end
-# end
+end
