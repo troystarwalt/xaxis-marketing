bbe7b143600f30539c336d1ebf861edff835ca62
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-22 11:34:08 -0400

    updates migration file

--- db/migrate/20160906150622_create_posts.rb
+++ db/migrate/20160906150622_create_posts.rb
@@ -1,6 +1,6 @@
-class CreateUpdates < ActiveRecord::Migration[5.0]
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :updatess do |t|
+    create_table :posts do |t|
       t.string :title, :null => false
       t.text :text, :null => false
       t.string :author
