dfd132b1b2be2cbdf38cc01dc2e8181247bb110a
Matthew Costabile <mattcosta7@gmail.com>
2016-09-26 16:50:26 -0400

    adds smooth scrollin and goldfish easter-egg

---
+++ app/assets/javascripts/smooth_scroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("a[href^='#'][class*='smooth-scroll']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 150
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
