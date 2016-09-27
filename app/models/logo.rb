030d8f504cde9f7364161964e682602fc2c572d1
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-27 15:30:41 -0400

    adds download funcationality to logos

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,4 +1,7 @@
 class Logo < ApplicationRecord
   belongs_to :brand
+  # Every logo must have a name (ie file name), be connected to a brand, and have an attachment
+  validates :name, :brand_id, :file, :presence => true
+
   mount_uploader :file, FileUploader
 end
