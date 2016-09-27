caf97628db37a38ce96d7ad48ae2f11317eae0b6
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-27 13:09:12 -0400

    adds admin signup process and emailing

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
+
   index do
     selectable_column
     id_column
@@ -19,9 +20,12 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
     end
     f.actions
   end
+
+
 end
+
+      # f.input :password
+      # f.input :password_confirmation
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,13 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create { |admin| admin.send_reset_password_instructions }
+
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+
+  def password_required?
+    new_record? ? false : super
+  end
 end
