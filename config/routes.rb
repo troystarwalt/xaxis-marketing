c007eaf68b7bfa5b94e9800e1e9bf76338b83609
Troy Starwalt <troy.starwalt@gmail.com>
2016-09-29 16:56:36 -0400

    fixes tag route to not interfer with post show path

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
   resources :brands, path: :brand_assets
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'posts/:tag', to: 'posts#index', as: :tag
   resources :posts
-
+  get 'tags/:tag', to: 'posts#index', as: :tag
   root :to => 'posts#main'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
