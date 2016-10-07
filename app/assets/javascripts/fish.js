4ba8298a7a551dfb6ba62e2eed7109fe2c712599
Matthew Costabile <mattcosta7@gmail.com>
2016-10-07 17:39:22 -0400

    updates fishy

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var kkeys = [], fishyText = [70,73,83,72,89]; //this spells dinner
   $(window).on("keydown", function(e){
     kkeys.push( e.keyCode );
-    if ( kkeys.toString() == fishyText.toString()) {
+    if ( kkeys.toString().includes(fishyText.toString()) ) {
       kkeys = [];
       swimFishySwim();
     }
@@ -10,10 +10,9 @@ $(document).ready(function(){
 });
 
 function swimFishySwim(){
-  $('body').append("<img src='/assets/goldfish.png' class='fish-image' />")
+  $('body').append("<img src='/assets/goldfish.png' class='fish-image' />");
   $('.fish-image').animate({
         left: "100vw"
-      }, 5000, function(){
-        $('.fish-image').remove();
-  });
+      }, 5000, function(){$('.fish-image').remove();
+    });
 }
