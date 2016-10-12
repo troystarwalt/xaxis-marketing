9c260b99dd6f8c331c639bffe568d16e5df5d3e8
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-12 13:12:15 -0400

    adds scroll indicator to side-nav

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -2,25 +2,23 @@
 
 $(document).ready( function () {
   var sideMenu = $('#side-nav'),
-  sideMenuHeight = sideMenu.outerHeight()+15
+  sideMenuHeight = sideMenu.outerHeight()+400
   menuItems = sideMenu.find("a"),
   scrollItems = menuItems.map(function () {
     var item = $($(this).attr("href"));
     if (item.length) { return item;}
   });
+  console.log(menuItems);
   $(window).scroll(function(){
-
+    console.log(sideMenuHeight + this);
     var fromTop = $(this).scrollTop()+sideMenuHeight;
-
+    console.log(fromTop);
     var cur = scrollItems.map(function(){
       if ($(this).offset().top<fromTop)
       return this;
     });
     cur = cur[cur.length-1]
     var id = cur && cur.length ? cur[0].id: "";
-
-    menuItems
-    .parent().removeClass('active')
-    .end().filter("[href='#"+id+"']").parent().addClass("active");
+    menuItems.parent().removeClass('active-side-link').end().filter("[href='#"+id+"']").parent().addClass("active-side-link");
   });
 });
