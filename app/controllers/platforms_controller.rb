656fe28e00bbf7a3b1757dff60eee9139ac6701a
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-17 17:04:31 -0400

    adds platform page for each platform

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -7,6 +7,22 @@ class PlatformsController < ApplicationController
 
   def show
     @platform = Platform.friendly.find(params[:id])
+    @spotlight_path = spotlight_path
+    @turbine_path = turbine_path
+    @xanadu_path = xanadu_path
   end
-  
+
+private
+  def spotlight_path
+  path = Platform.find_by(slug: :spotlight)
+  end
+
+  def turbine_path
+  path = Platform.find_by(slug: :turbine)
+  end
+
+  def xanadu_path
+    path = Platform.find_by(slug: :xanadu)
+  end
+
 end
