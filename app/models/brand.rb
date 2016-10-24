0a36fcee67a9e7396870af7ed0e845f8dcfcbac2
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-24 13:53:17 -0400

    updates pres hub with download links

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,4 +4,9 @@ class Brand < ApplicationRecord
   accepts_nested_attributes_for :brand_accessories, :allow_destroy => true
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
+
+  def self.last_of_a_category(brand, category)
+    this_brand = Brand.find_by_name(brand)
+    this_brand.brand_accessories.where(category: category).last
+  end
 end
--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -41,7 +41,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(zip pdf ppt pptx doc docx ase png eps jpeg jpg)
+    %w(zip pdf ppt pptx doc docx ase png eps jpeg jpg dotx)
   end
 
   # Override the filename of the uploaded files:
