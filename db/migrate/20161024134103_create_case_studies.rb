0b02e572578e4be825dc179a2d2eaf6250ed0aee
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-24 16:30:57 -0400

    fixes migrations

--- db/migrate/20161024134103_create_case_studies.rb
+++ db/migrate/20161024134103_create_case_studies.rb
@@ -1,13 +1,14 @@
 class CreateCaseStudies < ActiveRecord::Migration[5.0]
-  def change
-    create_table :case_studies do |t|
-      t.references :platform, foreign_key: true
-      t.string :author
-      t.datetime :release_date
-      t.text :short_description
-      t.string :pdf_attachment
-      t.string :title
-      t.timestamps
-    end
-  end
+  # Had to comment this out and then create a new migration to drop the prevous case study table.
+  # def change
+  #   create_table :case_studies do |t|
+  #     t.references :platform, foreign_key: true
+  #     t.string :author
+  #     t.datetime :release_date
+  #     t.text :short_description
+  #     t.string :pdf_attachment
+  #     t.string :title
+  #     t.timestamps
+  #   end
+  # end
 end
