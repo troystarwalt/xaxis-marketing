12c4e148b642eac0612e2d6a5e8081179b59a8bb
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-27 11:53:14 -0400

    fucking hell

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -29,8 +29,6 @@ CarrierWave.configure do |config|
 
 
   config.fog_directory  = 'xaxis-internal-stage'               # required
-    config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
