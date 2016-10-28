b6535c1980c36b5246a8d8981c56b534e34f74c3
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-28 15:37:58 -0400

    updates port for production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
     config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
