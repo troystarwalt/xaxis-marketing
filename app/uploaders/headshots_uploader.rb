acb5c9735e183b6137a78cca05a33cda187820cf
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-28 14:17:41 -0400

    adds bulk download

--- app/uploaders/headshots_uploader.rb
+++ app/uploaders/headshots_uploader.rb
@@ -14,6 +14,14 @@ class HeadshotsUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
+    def default_url(*args)
+      # For Rails 3.1+ asset pipeline compatibility:
+      # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+      random = ["xaxis", "lr", "turbine", "plista", "triad"]
+      "https://robohash.org/#{random.sample}.png?size=200x200"
+    end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -32,18 +40,24 @@ class HeadshotsUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [200, 200]
+
+  version :thumb, :if => :image? do
+    process resize_to_limit: [200, 200]
   end
 
-  version :large do
+  version :large, :if => :image? do
     process resize_to_fit: [1024, 768]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg png)
+  end
+
+  # We are randomizing the name of the file here to protect against duplication.
+  def filename
+      "#{model.last_name.parameterize}-#{secure_token(10)}.#{file.extension}" if original_filename.present?
   end
 
   # Override the filename of the uploaded files:
@@ -51,5 +65,16 @@ class HeadshotsUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+    protected
+
+    def secure_token(length=16)
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+    end
+
+    def image?(new_file)
+      new_file.content_type.start_with? 'image'
+    end
 
 end
