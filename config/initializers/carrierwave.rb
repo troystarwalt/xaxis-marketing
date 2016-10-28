361e9e048e55ffbe239bc4c70784b1384d3b845b
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-28 14:30:53 -0400

    moves fog config to production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,20 +12,22 @@ CarrierWave.configure do |config|
     CarrierWave.configure do |config|
       config.storage = :fog
       config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
+        config.fog_credentials = {
+          :provider               => 'AWS',                             # required
+          :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+          :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],     # required
+          :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
+        }
+        config.fog_directory  = 'xaxis-internal-stage'               # required
+        #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+        #config.fog_public     = false                                  # optional, defaults to true
+        config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
   end
 
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],     # required
-    :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'xaxis-internal-stage'               # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
 end
 
 module CarrierWave
