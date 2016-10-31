3dd68588018330dda17af45229372fe3a5dfdacd
Matthew Costabile <mattcosta7@gmail.com>
2016-10-31 15:23:46 -0400

    updates searching for industries

--- app/admin/case_study.rb
+++ app/admin/case_study.rb
@@ -4,21 +4,8 @@ ActiveAdmin.register CaseStudy do
 
   before_save do |model|
     puts "casing"
-    puts model.industry_list
     model.industry_list = model.industry_list.map{|industry| industry.capitalize}.join(',')
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
   form :html => { :multipart => true } do |f|
     f.semantic_errors *f.object.errors.keys
