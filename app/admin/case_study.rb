cf1ef01c287f79dd04c416e5e49c6a173cbc0f88
Matthew Costabile <mattcosta7@gmail.com>
2016-10-31 14:59:43 -0400

    adds industry tags to case studies

--- app/admin/case_study.rb
+++ app/admin/case_study.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register CaseStudy do
 
-  permit_params :title, :pdf_attachment, :platform_id, :author, :release_date, :short_description
+  permit_params :title, :pdf_attachment, :platform_id, :author, :release_date, :short_description, :industry_list
+
+  before_save do |model|
+    puts "casing"
+    puts model.industry_list
+    model.industry_list = model.industry_list.map{|industry| industry.capitalize}.join(',')
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -23,9 +29,12 @@ ActiveAdmin.register CaseStudy do
       f.input :short_description, placeholder: "Short Description for main case studies page"
       f.input :author, as: "hidden", :input_html => { value: f.current_admin_user.email }
       f.input :pdf_attachment, as: :file
+      f.input :industry_list, label: "Industries <i>Separate With Commas</i>".html_safe, input_html: {value: f.object.industry_list.join(", ")}
     end
     f.actions
   end
+
+
   show do
     attributes_table do
       row :title
@@ -38,6 +47,9 @@ ActiveAdmin.register CaseStudy do
         f.pdf_attachment_identifier
       end
       row :platform
+      row :industries do |f|
+        f.industry_list
+      end
       row :created_at
       row :updated_at
     end
--- app/models/case_study.rb
+++ app/models/case_study.rb
@@ -2,6 +2,7 @@ class CaseStudy < ApplicationRecord
   belongs_to :platform
   mount_uploader :pdf_attachment, FileUploader
   self.per_page = 6
+  acts_as_taggable_on :industries
 
   def get_pretty_release_date
     release_date.to_date.strftime("%m\/\%d\/\%Y")
