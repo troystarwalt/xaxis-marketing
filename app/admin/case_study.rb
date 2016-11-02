a13e55781e37683c987603b5899a505ce5bc4526
Matthew Costabile <mattcosta7@gmail.com>
2016-11-02 11:44:54 -0400

    updates admin to account for regions

--- app/admin/case_study.rb
+++ app/admin/case_study.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register CaseStudy do
 
-  permit_params :title, :pdf_attachment, :platform_id, :author, :release_date, :short_description, :industry_list
+  permit_params :title, :pdf_attachment, :platform_id, :author, :release_date, :short_description, :industry_list, region_list: []
 
   form :html => { :multipart => true } do |f|
     f.semantic_errors *f.object.errors.keys
@@ -12,6 +12,7 @@ ActiveAdmin.register CaseStudy do
       f.input :author, as: "hidden", :input_html => { value: f.current_admin_user.email }
       f.input :pdf_attachment, as: :file
       f.input :industry_list, label: "Industries <i>Separate With Commas</i>".html_safe, input_html: {value: f.object.industry_list.join(", ")}
+      f.input :region_list, as: :select, input_html: {multiple: true}, collection: CaseStudy.region_list
     end
     f.actions
   end
@@ -32,10 +33,34 @@ ActiveAdmin.register CaseStudy do
       row :industries do |f|
         f.industry_list
       end
+      row :regions do |f|
+        f.region_list
+      end
       row :created_at
       row :updated_at
     end
     active_admin_comments
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :author
+    column :release_date
+    column :short_description
+    column :pdf_attachment do |f|
+      f.pdf_attachment_identifier
+    end
+    column :platform
+    column :industries do |f|
+      f.industry_list
+    end
+    column :regions do |f|
+      f.region_list
+    end
+    column :created_at
+    column :updated_at
+    actions
   end
 
 end
--- app/models/case_study.rb
+++ app/models/case_study.rb
@@ -4,14 +4,24 @@ class CaseStudy < ApplicationRecord
   mount_uploader :pdf_attachment, FileUploader
   self.per_page = 6
   acts_as_taggable_on :industries, :regions
-  before_save :capitalize_tags
+  before_save :capitalize_tags, :normalize_region
   pg_search_scope :search_for, against: %w(title searchable_pdf_text), using: { tsearch: { any_word: true } }
+
   def self.get_industry_tags_for_select
     all.map{|case_study| case_study.industries.map{|industry| industry.name }}.flatten.sort.uniq
   end
 
   def self.get_region_tags_for_select
     all.map{|case_study| case_study.regions.map{|region| region.name }}.flatten.sort.uniq
+  end
+
+  def self.region_list
+    [
+      'APAC',
+      'EMEA',
+      'Latam',
+      'North America'
+    ].freeze
   end
 
   def get_pretty_release_date
@@ -22,6 +32,9 @@ class CaseStudy < ApplicationRecord
     self.industry_list = self.industry_list.map{|industry| industry.capitalize}.join(',')
   end
 
-
+  def normalize_region
+    puts 'regions'
+    puts self.region_list
+  end
 
 end
