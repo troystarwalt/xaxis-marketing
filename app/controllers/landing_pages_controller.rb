14df980e18daf56b78c6afd821e86899532eef7c
Matthew Costabile <mattcosta7@gmail.com>
2016-11-03 12:17:31 -0400

    brings calculator into main application

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,16 @@
+class LandingPagesController < ApplicationController
+
+  def value_calculator
+    @title = "Xaxis | Value Calculator"
+    @javascript_file_overwrite_name = 'landing_pages/value_calculator.js'
+    @css_files_overwrite = 'landing_pages/value_calculator'
+
+  end
+
+  def expert_interview_hub
+    @title = "Xaxis | Expert Interview Hub"
+    @javascript_file_overwrite_name = ''
+    @css_files_overwrite = ['expert_interview_hub']
+  end
+
+end
---
+++ test/controllers/landing_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class LandingPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get value_calculator" do
+    get landing_pages_value_calculator_url
+    assert_response :success
+  end
+
+  test "should get expert_interview_hub" do
+    get landing_pages_expert_interview_hub_url
+    assert_response :success
+  end
+
+end
