ee4286ab779e68d3ff82f8e2e4d9bee09ede8a43
Troy Starwalt <troy.starwalt@gmail.com>
2016-12-07 17:35:44 -0500

    adds headshot modal

---
+++ app/assets/javascripts/brand-modal.js
@@ -0,0 +1,22 @@
+// $('.headshot-image').on("click", function() {
+//   $('#hs_lightbox_window').toggleClass('closed');
+//   e.preventDefault();
+// })
+$(document).ready(function(){
+  $('.headshot-image').on('click', function (e){
+    var index = $(this).data('index');
+    var headshot = gon.headshots[index];
+
+    $('.hs-img').attr({
+      src: headshot.image.large.url,
+      alt: headshot.first_name,
+      photo_id: headshot.id
+      });
+    $('#hsModal').modal('show');
+    e.preventDefault();
+  });
+
+  $('#hsModal').on('hide.bs.modal', function(e){
+    $('.hs-img').attr('src', '');
+  })
+});
