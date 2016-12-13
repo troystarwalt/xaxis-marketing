2124aa35e6d1145b928aa688f8eb5c470967b32a
Troy Starwalt <troy.starwalt@gmail.com>
2016-12-13 18:35:31 -0500

    adds all content for brands with json

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -47,4 +47,117 @@ module BrandsHelper
     end
   end
 
+  def content_for_brands
+    {xaxis: {
+      welcome: "<p>Welcome to the home of all Xaxis brand assets! We have a few guidelines for using the brand resources to ensure our brand is accurately represented.</p>
+      <p>You can download individual assets by choosing from the menu on the left-hand side, or you can download all the brand resources as a zip file.</p> <p>We hope this identity system inspires you to create brilliant materials for the Xaxis brand.</p>",
+      logo: "<p>The vertical Xaxis logo lock-up is the preferred arrangement, and is available in six variations. In most cases, and whenever possible, use the 4-color version. The negative version of the logo should only be used on a color background.</p>
+      <p>The horizontal Xaxis logo is to be used for smaller applications and endorsements. It is available in six variations. In most cases, and whenever possible, use the 4-color version. The negative version of the logo should only be used on a color background.</p>
+      <p>For detailed logo usage, please refer to the Xaxis Brand Guidelines.</p>",
+      font: "<p>Gotham is the official typeface of Xaxis. It is used in four weights–Extra Light, Book, Medium and Bold. For online and web-based communications, please use Arial for all live copy.</p>",
+      images: "<p>Choosing the right image to communicate the brand values and message is very important. The images should also be futuristic and tech-driven. Avoid using business cliché pictures.</p>
+      <p>The images should have at least three of these qualities:</p>
+      <ul>
+      <li>interesting angles</li>
+      <li>contrast</li>
+      <li>style</li>
+      <li>movement</li>
+      <li>depth</li>
+      <li>audience faces</li>
+      </ul>
+      <p>Images used in Powerpoint as a background should have an overlay layer of the color #1D3647 at 30% opacity. If you are unsure of how to adjust the layer opacity, just copy and paste the overlay you see on the cover slide in the template!</p>",
+      ppt: "<p>The new Xaxis Powerpoint Template is updated with countless selections of icons, layouts and stock images that allow you to build beautiful slides with just a few clicks!</p>
+      <p>Remember, always check back to the hub for the latest templates and updates!</p>",
+      word: "<p>Please make sure all external facing material is written in this template.</p>",
+      facts: "<p>Xaxis Fast Facts is a document that contains boilerplates for all Xaxis portfolio brands and statistics such as, number of employees, number of offices, and revenue. This document is updated on a quarterly basis by the Global Marketing team.</p>",
+      capa: "<p>The Xaxis Global Capabilities deck includes Xaxis’ core capabilities, corporate structure, and an overview of our products and services. It provides clients with a deep understanding of what Xaxis has to offer as well as our overall mission and objectives.</p>"
+    },
+    triad: {
+      welcome: "<p>Welcome to Triad Retail Media brand resources center!</p>
+
+      <p>This guideline provides the appropriate usage and construction of various Triad Retail Media (TRM) identity elements. Use these elements to accurately communicate our brand. We worked hard to develop this brand for you to be proud of.
+      </p>
+      <p>You can download individual assets by choosing from the menu on the left-hand side, or you can download all the brand resources as a zip file.</p>",
+      logo: "<p>The logo is a unique piece of artwork and must never be modified or altered in any way without prior approval. The preferred version of the logo is the vertical orientation; however, if a horizontal version is necessary, the secondary, non-stacked version may be used.</p>
+
+      <p>The Triad Retail Media brand mark (with or without tagline) is to be used in all internal and external deliverables. This includes correspondence, proposals, contracts, editorial strategies, creative decks, and any other documentation that represents the brand and voice of Triad. </p>
+
+      <p>For detailed logo usage, please refer to the Triad Brand Guidelines.
+      </p>",
+      font: "<p>The official typefaces chosen to represent the Triad Retail Media brand are Magna EF Light and Trade Gothic LT Std. With its sharp edges and clarity of characters, Magna EF Light presents the Triad name as a trusted leader.</p>
+
+      <p>The official typefaces chosen to represent the Triad Retail Media brand in print is Lato. Lato should be the standard font choice when creating print materials. However, if a larger font family is needed, Helvetica Neue LT STD maybe be used.</p>
+
+      <p>To accommodate all different print materials, a large font family that is legible, clean and modern (like Helvetica Neue LT Std) may be used to create a professional and cohesive look on Triad Retail Media’s print material.
+      </p>",
+      images: "<p>Choosing the right image to communicate the brand values and message is very important.</p>
+      <p>The images should have at least three of these qualities:</p>
+      <ul>
+      <li>interesting angles</li>
+      <li>contrast</li>
+      <li>style</li>
+      <li>movement</li>
+      <li>depth</li>
+      <li>audience faces</li>
+      </ul>
+      <p>Images used in Powerpoint as a background should have an overlay layer of the color #1D3647 at 30% opacity. If you are unsure of how to adjust the layer opacity, just copy and paste the overlay you see on the cover slide in the template!</p>",
+      ppt: "<p></p>",
+      word: "<p></p>",
+      facts: "<p></p>",
+      capa: "<p>The Triad Global Capabilities Deck are available for download in both Triad template and Xaxis template.</p>"
+    },
+    plista: {
+      welcome: "<p>Welcome to plista brand resources center! plista is our native advertising and content distribution platform.</p>
+
+      <p><strong>plista should always written in lower case letter, without exception.</strong>
+        </p>
+        <p>You can download individual assets by choosing from the menu on the left-hand side, or you can download all the brand resources as a zip file.</p>",
+        logo: "<p>The primary plista logo should be used on a white background. The reversed white logo can be used on colored background.</p>",
+        font: "<p>plista uses the Calibri font for use in the office and in presentations. The font is a component of Microsoft Office for Windows and Apple Macintosh and does not need to be downloaded.</p>
+
+        <p>The font Myriad Pro is the standard font for plista GmbH print media.</p>
+
+        <p>For web design the Google Web Font Lato was chosen.</p>",
+        images: "<p>The plista images should have a lot of green and natural feel to it. They should be native, authentic and welcoming. The overall tone should be green for nature, and blue for the ocean.</p>
+        <p>The images should have at least three of these qualities:</p>
+        <ul>
+        <li>interesting angles</li>
+        <li>contrast</li>
+        <li>style</li>
+        <li>movement</li>
+        <li>depth</li>
+        <li>audience faces</li>
+        </ul>
+        <p>Images used in Powerpoint as a background should have an overlay layer of the color #1D3647 at 30% opacity. If you are unsure of how to adjust the layer opacity, just copy and paste the overlay you see on the cover slide in the template!</p>",
+        ppt: "<p>The plista Powerpoint template has countless selections of icons, layouts and stock images that allow you to build beautiful slides with just a few clicks!</p>
+        <p>Remember, always check back to the hub for the latest templates and updates!</p>",
+        word: "<p>Please make sure all external facing material is written in this template.</p>",
+        facts: "<p>plista Fast Facts is a document that contains boilerplates for all Xaxis portfolio brands and statistics such as, number of employees, number of offices, and revenue. This document is updated on a quarterly basis by the Global Marketing team.</p>",
+        capa: "<p>For plista, there is a Global Capabilities Deck for Advertiser and a Global Capabilities Deck for Publisher. Please make sure you select the suitable one for your use. </p>"
+      },
+      lightreaction: {
+        welcome: "<p>At Light Reaction, we understand the science behind consumer behavior. We embrace the power of performance — performance in the form of actions that drive guaranteed business outcomes and grow revenue. We strive to make advertising accountable and remove risk for our clients. We believe in performability.</p>
+        <p>You can download individual assets by choosing from the menu on the left-hand side, or you can download all the brand resources as a zip file.</p>",
+        logo: "<p>Both the primary and horizontal logos are available in three different color treatments, which need to be chosen according to how and where you plan to use the logo.</p>",
+        font: "<p>Typography plays a significant role in Light Reaction’s visual system. Universe LT STD is the primary font family of Light Reaction. It should be used in three different weights: Light, Roman and Bold.</p>
+        <p>For cases where Universe is not available, Arial can be used in Regular and Bold weights.</p>",
+        images: "<p>Choosing the right image to communicate the brand values and message is very important. The Light Reaction images should have heavy use of yellow beam lights, and the overall tone should be warm.</p>
+        <p>The images should have at least three of these qualities:</p>
+        <ul>
+        <li>interesting angles</li>
+        <li>contrast</li>
+        <li>style</li>
+        <li>movement</li>
+        <li>depth</li>
+        <li>audience faces</li>
+        </ul>
+        <p>Images used in Powerpoint as a background should have an overlay layer of the color #1D3647 at 30% opacity. If you are unsure of how to adjust the layer opacity, just copy and paste the overlay you see on the cover slide in the template!</p>",
+        ppt: "<p>The new Light Reaction Template is updated with countless selections of icons, layouts and stock images that allow you to build beautiful slides with just a few clicks!</p>
+        <p>Remember, always check back to the hub for the latest templates and updates!</p>",
+        word: "<p>Please make sure all external facing material is written in this template.</p>",
+        facts: "<p>Light Reaction Fast Facts is a document that contains boilerplates for all Xaxis portfolio brands and statistics such as, number of employees, number of offices, and revenue. This document is updated on a quarterly basis by the Global Marketing team.</p>",
+        capa: "<p>The Light Reaction Global Capabilities Deck includes Light Reaction’s mission statement, core capabilities and product offerings.</p>"
+      }
+    }
+  end
 end
