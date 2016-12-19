54b863c49c81e788f72a53efca5925abe63e5b61
Troy Starwalt <troy.starwalt@gmail.com>
2016-12-19 13:42:57 -0500

    fixes mobile nav on active admin and updates 101 link

--- app/assets/javascripts/admin_nav.js
+++ app/assets/javascripts/admin_nav.js
@@ -1,13 +1,15 @@
 $('.active_admin .logged_in').ready(function() {
-  $('.site_title').append("<button class='mobile-nav'>X</button>");
+  $('.site_title').append("<button class='mobile-nav'><i class='fa-toggler fa fa-bars'></i></button>");
 
   $('.mobile-nav').on('click', function () {
     var menu_items = $('#tabs');
     if (menu_items.css('display') =='none') {
       menu_items.css('display', 'flex');
+      $('.fa-toggler').toggleClass('fa-bars fa-times');
     }
     else {
       menu_items.toggle();
+      $('.fa-toggler').toggleClass('fa-times fa-bars');
     }
   });
 });
