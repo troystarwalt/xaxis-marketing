c8ece9ff9a449228a446c9b5d63be230652e1953
Troy Starwalt <troy.starwalt@gmail.com>
2017-01-03 16:01:55 -0500

    adds bios modal and styling

--- app/assets/javascripts/brand-modal.js
+++ app/assets/javascripts/brand-modal.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // Using gon variables for image modals
   $('.headshot-image').on('click', function (e){
     var index = $(this).data('index');
     var headshot = gon.headshots[index];
@@ -11,8 +12,24 @@ $(document).ready(function(){
     $('#hsModal').modal('show');
     e.preventDefault();
   });
-
+  // Clearing out the image from the modal on hide/close
   $('#hsModal').on('hide.bs.modal', function(e){
     $('.hs-img').attr('src', '');
-  })
+  });
+
+  // Using gon variable for bio modals
+  $('.headshot-bio').on('click', function (e) {
+    var index = $(this).data('index');
+    var hsBio = gon.headshots[index];
+    $('.hs-bio-img').attr({
+      src: hsBio.image.thumb.url,
+      alt: hsBio.full_name,
+      photo_id: hsBio.id
+    });
+    $('.hs-name').html(hsBio.first_name + " " + hsBio.last_name);
+    $('.hs-title').html(hsBio.title);
+    $('.hs-bio').html(hsBio.bio);
+    $('#hsBioModal').modal('show');
+    e.preventDefault();
+  });
 });
