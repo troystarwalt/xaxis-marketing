6bb402108a6b346b70864dd47719dfd85b4cd041
Troy Starwalt <troy.starwalt@gmail.com>
2017-01-11 11:27:56 -0500

    adds navigation animation

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -104,3 +104,20 @@ $('#brands').ready(function() {
     checkOffset();
   });
 });
+
+function init() {
+  window.addEventListener('scroll', function(e) {
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrink_on = 150,
+        body_element = document.querySelector("body");
+    if (distanceY > shrink_on) {
+      body_element.classList.add('shrnk');
+    } else {
+      if (body_element.classList.contains('shrnk')) {
+        body_element.classList.toggle('shrnk');
+      }
+    }
+  });
+}
+
+window.onload = init();
