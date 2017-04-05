41857032bd5ffabf437bd7db9b2b6b30a90b06bc
Troy Starwalt <troys@troys.local>
2017-04-05 12:52:04 -0400

    adds one_xaxis, adds ajax, adds facts

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,6 +2,7 @@
 class BrandsController < InheritedResources::Base
   require 'zip'
   actions :index, :show   # This tells InheritedResources to only use index and show.
+  helper_method :remove_dash_make_symbol
 
 
   def index
@@ -10,14 +11,17 @@ class BrandsController < InheritedResources::Base
   end
 
   def show
-    brand_collection = Brand.includes(:headshots, :brand_accessories)
-    @brand = brand_collection.friendly.find(params[:id]) #Get Brand from params
-    brand_for_content = @brand.slug.gsub('-','').to_sym   #if brand has a -, then remove it
-    @content = helpers.content_for_brands[brand_for_content.to_sym]  # Pull in text content for each brand
-    gon.headshots = @brand.headshots.ordered_by_priority
-    @image_bank = GlobalAccessory.where(category: 'image_bank').last  # Get the most recent image bank download
-    brand_accessories_zip = @brand.brand_accessories.where(category: ['guidelines', 'logo', 'palette'])
-    @pr_kit = GlobalAccessory.where(category: 'pr_kit').last  # Get the brands pr kit
+    if params[:id] == "one-xaxis"
+      @brand = Brand.includes(:brand_accessories).friendly.find(params[:id])
+      get_content()
+    else
+      @brand = Brand.includes(:headshots, :brand_accessories).friendly.find(params[:id])
+      gon.headshots = @brand.headshots.ordered_by_priority
+      @image_bank = GlobalAccessory.where(category: 'image_bank').last  # Get the most recent image bank download
+      brand_accessories_zip = @brand.brand_accessories.where(category: ['guidelines', 'logo', 'palette'])
+      @pr_kit = GlobalAccessory.where(category: 'pr_kit').last  # Get the brands pr kit
+    end
+    @content = helpers.content_for_brands[remove_dash_make_symbol(@brand.slug)]  # Pull in text content for each brand
 
     # Take the assets and zip them up for the user.
     respond_to do |format|
@@ -37,8 +41,23 @@ class BrandsController < InheritedResources::Base
     end
   end
 
-  private
+  def facts
+    @selected = Brand.find(params[:brand_id])
+    @other_content = helpers.content_for_brands
+    respond_to do |format|
+      format.js
+    end
+  end
+  protected
+    def get_content
+      @other_content = helpers.content_for_brands
+    end
 
+    def remove_dash_make_symbol(s)
+      s.gsub('-', '').to_sym
+    end
+
+  private
     def brand_params
       params.require(:brand).permit(:name)
     end
