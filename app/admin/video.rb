7a7bb7e342c6f6c2a25e966a71a740ac6e5aa9fe
Troy Starwalt <troys@troys.local>
2017-04-07 16:40:46 -0400

    adds box download link for videos and updates homepage boxes

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 ActiveAdmin.register Video do
   menu parent: "Global Accessories"
-  permit_params :name, :description, :direct_link, :embed_link, :image_preview, :platform_id, :brand_id
+  permit_params :name, :description, :direct_link, :embed_link, :image_preview, :platform_id, :brand_id, :box_download_link
 
   # before_save do |video|
   #   get_link = VideoThumb::get(video.direct_link)
@@ -18,6 +18,7 @@ ActiveAdmin.register Video do
       link_to f.direct_link, f.direct_link, target: "_blank"
     end
     column :embed_link
+    column :box_download_link
     column :image_preview do |f|
       if f.image_preview.present?
         image_tag(f.image_preview, class: 'admin_show_image')
@@ -33,8 +34,9 @@ ActiveAdmin.register Video do
     f.inputs "Create a New video" do
       f.input :brand, as: :radio  # Can add platforms back like this.
       f.input :name, placeholder: "Video Name"
-      f.input :description, hint: "Not required, but can be useful for you."
-      f.input :direct_link, placeholder: "https://vimeo.com/149429953"
+      f.input :description, hint: "Not required, but it can be useful for you."
+      f.input :direct_link, placeholder: "https://vimeo.com/149429953", hint: "Use the basic vimeo link."
+      f.input :box_download_link, placeholder: "https://xaxismarketing.box.com/s/dyonll3dqavgofubvm04lp77cz0zk4sr", hint: "Make sure to put the correct sharing link for others here."
       f.input :image_preview, as: :file, hint: "Upload image here. If you don't have one, we'll try and grab an image from Vimeo."
     end
     f.actions
@@ -46,6 +48,8 @@ ActiveAdmin.register Video do
       row :description
       row :direct_link
       row :embed_link
+      row :box_download_link
+      row :vimeo_video_id
       row :image_preview do |f|
         if f.image_preview.present?
           image_tag(f.image_preview, class: 'admin_show_image')
