c5d0702ef1a745bcfc435a376605df8ad10b9aef
Troy Starwalt <troys@troys.local>
2017-04-21 09:52:57 -0400

    ready for prod

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -1,20 +1,6 @@
 ## Configure Rack CORS Middleware, so that CloudFront can serve our assets.
 ## See https://github.com/cyu/rack-cors
 if defined? Rack::Cors
-  puts "dinosaur-------------------------------------------------------------"
-    # Rails.configuration.middleware.insert_before 0, Rack::Cors do
-    #     allow do
-    #         origins %w[
-    #             https://marketing.xaxis.com
-    #              http://marketing.xaxis.com
-    #             https://www.marketing.xaxis.com
-    #              http://www.marketing.xaxis.com
-    #             https://xaxis-internal-staging.herokuapp.com
-    #              http://xaxis-internal-staging.herokuapp.com
-    #         ]
-    #         resource '/assets/*'
-    #     end
-    # end
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
